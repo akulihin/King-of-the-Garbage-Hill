77a39a2ba5b964aeb3deaf7c95887a945915e1b1
Artem Kulihin <duraek@gmail.com>
2019-02-15 14:35:54 -0500

    Update AccountSettings.cs

--- King-of-the-Garbage-Hill/LocalPersistentData/UsersAccounts/AccountSettings.cs
+++ King-of-the-Garbage-Hill/LocalPersistentData/UsersAccounts/AccountSettings.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace King_of_the_Garbage_Hill.LocalPersistentData.UsersAccounts
 {
