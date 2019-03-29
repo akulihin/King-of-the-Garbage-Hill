828db8ed2d36afc3c6387955e5b7d12692b10cbd
Artem Kulihin <duraek@gmail.com>
2019-03-29 19:09:50 -0400

    bug fix

--- King-of-the-Garbage-Hill/Game/DiscordMessages/GameUpdateMess.cs
+++ King-of-the-Garbage-Hill/Game/DiscordMessages/GameUpdateMess.cs
@@ -125,6 +125,7 @@ namespace King_of_the_Garbage_Hill.Game.DiscordMessages
         public string LeaderBoard(DiscordAccountClass discordAccount, CharacterClass character)
         {
             var game = _global.GamesList.Find(x => x.GameId == discordAccount.GameId);
+            if (game == null) return "ERROR 404";
             var players = "";
             var playersList = game.PlayersList;
 
