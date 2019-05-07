007fc98c23f54ffbc94d75e652befba1350740d1
Artem Kulihin <duraek@gmail.com>
2019-05-07 11:39:37 -0400

    Ah shit, here we go again.

--- King-of-the-Garbage-Hill/Game/GameLogic/CharacterPassives.cs
+++ King-of-the-Garbage-Hill/Game/GameLogic/CharacterPassives.cs
@@ -721,7 +721,7 @@ namespace King_of_the_Garbage_Hill.Game.GameLogic
                             await _phrase.DarksciFuckThisGame.SendLog(player);
 
                             if(game.RoundNo == 9 || game.RoundNo == 10 && !game.GetAllGameLogs().Contains("Нахуй эту игру"))
-                            game.AddPreviousGameLogs(
+                                game.AddPreviousGameLogs(
                                     $"**{player.DiscordAccount.DiscordUserName}:** Нахуй эту игру..");
                         }
                         //end Да всё нахуй эту игру:
