a7c59defd5ac95973b9788dba6a73f3dd096f85a
Artem <duraek@gmail.com>
2022-01-12 00:25:22 -0500

    new random

--- King-of-the-Garbage-Hill/Helpers/SecureRandom.cs
+++ King-of-the-Garbage-Hill/Helpers/SecureRandom.cs
@@ -4,15 +4,8 @@ using System.Threading.Tasks;
 
 namespace King_of_the_Garbage_Hill.Helpers;
 
-public sealed class SecureRandom : IServiceTransient
+public class SecureRandom : IServiceTransient
 {
-    private readonly RNGCryptoServiceProvider _csp;
-
-    public SecureRandom()
-    {
-        _csp = new RNGCryptoServiceProvider();
-    }
-
     public Task InitializeAsync()
     {
         return Task.CompletedTask;
@@ -21,33 +14,18 @@ public sealed class SecureRandom : IServiceTransient
     public int Random(int minValue, int maxExclusiveValue)
     {
         if (minValue == maxExclusiveValue) return minValue;
+        if (minValue > maxExclusiveValue)
+            throw new ArgumentOutOfRangeException($"{nameof(minValue)} must be lower than {nameof(maxExclusiveValue)}");
 
-        if (minValue >= maxExclusiveValue)
-            // ReSharper disable once NotResolvedInText its ok.
-            throw new ArgumentOutOfRangeException("minValue must be lower than maxExclusiveValue");
-        maxExclusiveValue += 1;
         var diff = (long)maxExclusiveValue - minValue;
         var upperBound = uint.MaxValue / diff * diff;
 
         uint ui;
         do
         {
-            ui = GetRandomUInt();
+            ui = BitConverter.ToUInt32(RandomNumberGenerator.GetBytes(24), 0);
         } while (ui >= upperBound);
 
         return (int)(minValue + ui % diff);
-    }
-
-    private uint GetRandomUInt()
-    {
-        var randomBytes = GenerateRandomBytes(sizeof(uint));
-        return BitConverter.ToUInt32(randomBytes, 0);
-    }
-
-    private byte[] GenerateRandomBytes(int bytesNumber)
-    {
-        var buffer = new byte[bytesNumber];
-        _csp.GetBytes(buffer);
-        return buffer;
     }
 }
