16b2bb7b59e165af932b63cd7cf5bbf338dba11f
DeepList <33755773+DeepList@users.noreply.github.com>
2022-08-25 23:43:18 +0300

    Update CharactersPhrases.cs

--- King-of-the-Garbage-Hill/Game/MemoryStorage/CharactersPhrases.cs
+++ King-of-the-Garbage-Hill/Game/MemoryStorage/CharactersPhrases.cs
@@ -381,6 +381,7 @@ public class CharactersUniquePhrase
         DeepListPokePhrase.PassiveLogRus.Add("Mit*SUKI*"); 
         DeepListPokePhrase.PassiveLogRus.Add("Почему гиены смеются? Потому что смешно!"); 
         DeepListPokePhrase.PassiveLogRus.Add("**Лол** коронный, **Кек** похоронный"); 
+        DeepListPokePhrase.PassiveLogRus.Add("Витамин C содержится в курице и __**БОБАХ**__"); 
 
         
 
